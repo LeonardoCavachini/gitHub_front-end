786d5c9160ddf10484c68d167c62573929a4aeea
Leonardo Cavachini <cavachini240@gmail.com>
2021-05-31 20:17:10 -0300

    done

--- src/Service/gitHubApi.js
+++ src/Service/gitHubApi.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 import axios from 'axios';
 
 const gitHubApi = axios.create({
-  baseURL: 'http://api.github.com',
+  baseURL: process.env.REACT_APP_API_GIT_URL || 'http://api.github.com',
 });
 
 export default gitHubApi;
