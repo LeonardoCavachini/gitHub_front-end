ca3755a74c5976a8d3a3cc1f4a42dc05eb54f79d
Leonardo Cavachini <cavachini240@gmail.com>
2021-05-31 19:44:11 -0300

    house kepping

--- src/Service/gitHubApi.js
+++ src/Service/gitHubApi.js
@@ -1,14 +1,7 @@
 import axios from 'axios';
 
-const data = {
-  client_id:"73e452b8483b248c51a0",
-  client_secret:"5e416eea06ea372b992809d291ccbff96060021d",
-}
-
 const gitHubApi = axios.create({
   baseURL: 'http://api.github.com',
 });
 
 export default gitHubApi;
-
-// https://github.com/login/oauth/authorize?scope=user,email&client_id=73e452b8483b248c51a0
